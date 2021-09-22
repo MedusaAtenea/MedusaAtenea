5d40ef2f944539f5dd3006254fdafcb3b3212200
Anlodx <anlodx1@gmail.com>
2021-09-22 15:17:18 -0500

    Agregamos menu de contacto

--- assets/js/events.js
+++ assets/js/events.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
     $("#bars").click(function(){
         $(".header .container_menu").toggleClass("show");
-    })
+    });
+
+    $("#contact_li").click(function(){
+        $(".header .contact_container").toggleClass("show_contact");
+    });
+    
+    $("#close_contact").click(function(){
+        $(".header .contact_container").removeClass("show_contact");
+    });
 });
