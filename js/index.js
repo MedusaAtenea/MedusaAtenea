9dcd7e9afcd8443d1dc40c603f4d719903febc03
Alonso Ramirez Paez <alonso.ramirez@salud.ssnl.gob.mx>
2021-09-23 18:11:06 -0500

    Caverna

--- js/index.js
+++ js/index.js
@@ -1,36 +1,39 @@
 window.addEventListener("scroll", () => {
     // let grass_layer_0 = document.getElementById();
     let grass_layer_0 = document.getElementById("grass__layer--0");
-    let grass_element = document.getElementById(); aqui me quede
+    let grass_element = document.getElementById("grass");
     let scrollY = window.scrollY;
     let sizeOfGrass = 1000;
     let amountOfPx = 1.5;
 
     let percentageToMove = 5;
 
-    let incrementInPercentage = 15;
+    let incrementInPercentage = 20;
     let initial = 10;
+
     
 
-    //grass_layer_0.getBoundingClientRect().top
 
-    //console.log(grass_layer_0.getBoundingClientRect().bottom)
+    
+    if (grass_element.getBoundingClientRect().top <= sizeOfGrass && grass_element.getBoundingClientRect().bottom > 0) {
+        let seenPixels = sizeOfGrass - grass_element.getBoundingClientRect().top;
+        
+        //A partir de 480px se va a parallax
+        if (seenPixels >= 480) {
+            let differential = (seenPixels - 480) / ((sizeOfGrass * 2) - 480); // 0 -> 1
+            let percentage = (initial - (differential * incrementInPercentage)) + "%";
 
-    //grass_layer_0
+            grass_layer_0.style.top = percentage;
+            console.log(percentage);
+        }
 
-    if (grass_layer_0.getBoundingClientRect().top <= sizeOfGrass && grass_layer_0.getBoundingClientRect().bottom > 0) {
-        //console.log("Estás visualizando el componente, y su efecto parallax")
-
-        let seenPixels = sizeOfGrass - grass_layer_0.getBoundingClientRect().top;
-        console.log(seenPixels);
-
-        let differential = seenPixels / (sizeOfGrass * 2); // 0 -> 1
-        let percentage = (initial - (differential * incrementInPercentage)) + "%";
+        
+        
         //let percentage = "-" + (differential * percentageToMove) + "%";
 
-        //grass_layer_0.style.top = percentage;
+        //
         //console.log(percentage);
     }
 
-    //console.log("Numero: " + scrollY + " Elemento: " + grass_layer_0.getBoundingClientRect().top)
+    
 })
